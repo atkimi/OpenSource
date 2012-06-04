ac89128a283a5128b9406883ecfa360760d51897
Mike Atkinson <mike.atkinson@newsint.co.uk>
2012-06-04 15:18:04 +0100

    Routine save, added prime function

--- Tools/python/test.py
+++ Tools/python/test.py
@@ -1,12 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 #
+# Well you know how it is with a new language ...
+# How do I concatonate strings? how do I cast integers to strings?
+# How do create and manage strings and arrays(lists)?
+# How do I test and extract command line arguments?
+#
+# This is my test  script to find out
+#
+#
+#
+def squareroot(value):
+    print "Iterating to obtain squareroot of  " + str(value)
+    loop=0
+    a=value/2
+    while loop < 10:
+        print "Trying Value " + str(a)
+        b=float(value/a)
+        a=float((b+a)/2)
+	if str(a) == str(b) : break
+#        loop+=1
+#
+# Primes, not that the else statement is associated with second for loop, NOT the if
+# Also should implement sieve of eratosthenes and skip even integers
+#
+def prime(upto):
+  print "Finding prime numbers up to " + str(upto)
+#  for a in range(2,upto):
+  for a in range(2,upto): 
+    for b in range(2,int(math.sqrt(a))):# sieve
+      if  a%b == 0 : break  
+    else:
+     print "Found Prime number %4.d" % a # formatted printing in python - add a printf placeholder like printf then a second arg "%" then the value
+#
+#
 # Fibonacci
 #
 #
-def fib():
+def fib(flist):
+    print "fibonacci numbers below " + str(flist)  
     a, b = 0, 1
-    while b < 10:
+    while b <= flist:
         print b
         a, b = b, a+b
 #
@@ -22,15 +56,18 @@ embedded in triple quotes "`'@!£%^&*()_~#|
 
 """
     print triplecodedstring
-    fib()
+    fib(100)
+    squareroot(100000)
+    prime(1000)
 
 ## _____________________
 ##
 ## E N T R Y   P O I N T
 ## _____________________
 ##
-import imp, sys, os, re, time
+import imp, sys, os, re, time, math
 identifier = "python-%s-%s" % (sys.version[:3], sys.platform)
 timestamp = time.strftime("%Y%m%dT%H%M%SZ", time.gmtime(time.time()))
+print "\nTime stamp is " + timestamp + "\nPython Version is " + identifier + "\n\n"
 if __name__ == "__main__":
     main()
